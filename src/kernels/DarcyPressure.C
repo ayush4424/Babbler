7af39e19541a55c03903d625288f9d6989b7ed87
ayush4424 <ayush4444@LAPTOP-HRF15JCJ>
2023-09-29 00:56:59 +0530

    implemented a zero-viscosity error and created tests for the Darcy pressure kernel

--- src/kernels/DarcyPressure.C
+++ src/kernels/DarcyPressure.C
@@ -8,24 +8,18 @@ DarcyPressure::validParams()
     InputParameters params = ADKernelGrad::validParams();
     params.addClassDescription("Compute the diffusion term for Darcy pressure ($p$) equation: "
         "$-\\nabla \\cdot \\frac{\\mathbf{K}}{\\mu} \\nabla p = 0$");
-    params.addRequiredParam<Real>("permeability", "The isotropic permeability ($K$) of the medium.");
-    params.addParam<Real>("viscosity", 7.98e-04,
-        "The dynamic viscosity ($\\mu$) of the fluid, the default value is that of water at 30"
-        "degree celcius (7.98e-04 Pa-s).");
-
     return params;
 }
 
 DarcyPressure::DarcyPressure(const InputParameters& parameters)
     : ADKernelGrad(parameters),
-
-    _permeability(getParam<Real>("permeability")),
-    _viscosity(getParam<Real>("viscosity"))
+    _permeability(getADMaterialProperty<Real>("permeability")),
+    _viscosity(getADMaterialProperty<Real>("viscosity"))
 {
 }
 
 ADRealVectorValue
 DarcyPressure::precomputeQpResidual()
 {
-    return (_permeability / _viscosity) * _grad_u[_qp];
+    return (_permeability[_qp] / _viscosity[_qp]) * _grad_u[_qp];
 }
